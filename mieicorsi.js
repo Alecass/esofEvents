0e20e6a731984c6e761780ed2e1b32b33c0a4346
Alessio Cassano <AlessioCass@MacBook-Pro-di-its.local>
2019-11-08 11:23:21 +0100

    prima versione corsi app

--- mieicorsi.js
+++ mieicorsi.js
@@ -1,31 +1,35 @@
-const motivoSpesa = document.querySelector('#motivo-spesa')
-const importoSpesa = document.querySelector('#importo-spesa')
+const nomeCorso = document.querySelector('#nome-corso')
+const punteggioCorso = document.querySelector('#punteggio-corso')
 
 const cancellaBtn = document.querySelector('#cancella-btn')
 const aggiungiBtn = document.querySelector('#aggiungi-btn')
 
-const speseList = document.querySelector('#list-spesa')
-
-const speseTotaliOutput = document.querySelector('#spese-totali')
-
-let spesaTotale = 0
+const corsiList = document.querySelector('#list-corsi')
 
 const alertController = document.querySelector('ion-alert-controller')
 
+const mediaOutput = document.querySelector('#media')
+
+let media = 0;
+let sommaPunteggi = 0;
+
+const messages = ['Per favore immettere un nome valido','Per favore immettere un valore compreso tra 0-5,perfavore immettere valori validi']
+
 const cancella = () =>{
-    motivoSpesa.value = ''
-    importoSpesa.value = ''
+    nomeCorso.value = ''
+    punteggioCorso.value = ''
 }
 
 cancellaBtn.addEventListener('click' ,cancella)
 aggiungiBtn.addEventListener('click', () => {
 
-    const motivoInserito = motivoSpesa.value
-    const importoInserito = importoSpesa.value
+    const nomeInserito = nomeCorso.value
+    const punteggioInserito = punteggioCorso.value
 
-    if(importoInserito<=0 || importoInserito.trim().lenght <=0 || motivoInserito.trim().lenght <= 0 ){
+    if(punteggioInserito <= 0 || punteggioInserito.trim().lenght <= 0  || punteggioInserito > 5 || nomeInserito.trim().lenght <= 0){
+        
         alertController.create({
-            message: 'Perfavore immettere valori validi',
+            message: messages[2],
             header: 'Valori non validi',
             buttons: ['ok']
         }).then(alertElem =>{
@@ -35,10 +39,18 @@ aggiungiBtn.addEventListener('click', () => {
     }
 
     const nuovoElem = document.createElement('ion-item')
-    nuovoElem.textContent = motivoInserito + ' : '+ importoInserito + '€'
-    speseList.appendChild(nuovoElem)
-// + variabile stringa la trasforma in numero
-    spesaTotale += +importoInserito
-    speseTotaliOutput.textContent = spesaTotale
+    nuovoElem.style="font-weight:bold"
+    nuovoElem.style.color="blue"
+    nuovoElem.textContent = nomeInserito + ' : ' + punteggioInserito
+    corsiList.appendChild(nuovoElem)
+
+    const star = document.createElement('ion-icon')
+    star.name = "star"
+    star.style.color = "orange"
+    nuovoElem.appendChild(star)
+
+    media += +punteggioInserito
+    sommaPunteggi += 1
+    mediaOutput.textContent = media/sommaPunteggi
     cancella()
 })
